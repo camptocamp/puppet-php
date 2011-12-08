607b3f073ea2515556833e3620354ba9336b6253
Bruce Morrison <bruce@stuffandcontent.com>
2011-12-08 11:17:53 +1000

    Add support for CentOS & Ubuntu.

--- manifests/pear.pp
+++ manifests/pear.pp
@@ -1,8 +1,8 @@
 define php::pear ($ensure="present") {
 
   case $operatingsystem {
-    Debian: { $phpprefix = "php-" }
-    RedHat: { $phpprefix = "php-pear-" }
+    Debian,Ubuntu: { $phpprefix = "php-" }
+    RedHat,CentOS: { $phpprefix = "php-pear-" }
     default: { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 
