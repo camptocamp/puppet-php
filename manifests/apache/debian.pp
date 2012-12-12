2c9ab75d851cffef61d71ba64388ef9a160ae99e
Raphaël Pinson <raphink@gmail.com>
2012-12-12 17:23:30 +0100

    Use before instead of notify for Apache::Module['php5'] in php::apache::debian

--- manifests/apache/debian.pp
+++ manifests/apache/debian.pp
@@ -1,12 +1,12 @@
-class php::apache::debian inherits php::apache::common {
+class php::apache::debian inherits ::php::apache::common {
 
-  Augeas["default php.ini settings"] {
-    incl => "/etc/php5/apache2/php.ini",
+  Augeas['default php.ini settings'] {
+    incl => '/etc/php5/apache2/php.ini',
   }
 
-  package { "libapache2-mod-php5": 
+  package { 'libapache2-mod-php5':
     ensure => present ,
-    notify => Apache::Module["php5"]
+    before => Apache::Module['php5'],
   }
 
 }
