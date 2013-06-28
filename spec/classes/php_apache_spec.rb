a04d9272c7ce90e0084204c99862dca2978287da
Geoff Davis <gadavis@ucsd.edu>
2013-06-28 15:17:52 -0700

    Refactor php::apache spec
    Includes checks for contents of implementation classes

--- spec/classes/php_apache_spec.rb
+++ spec/classes/php_apache_spec.rb
@@ -10,21 +10,42 @@ describe 'php::apache', :type => 'class' do
     it { should_not include_class('php::apache::redhat') }
   end
 
-  context 'on a RedHat osfamily' do
+  context 'on a supported osfamily' do
     let(:facts) { {
       :osfamily => 'RedHat',
       :operatingsystem => 'CentOS',
     } }
-    it { should include_class('php::apache::redhat') }
-    it { should_not include_class('php::apache::debian') }
-  end
+    it { should contain_augeas('default php.ini settings').with({
+      :lens => 'PHP.lns',
+      :changes => [
+        'set PHP/allow_url_fopen Off',
+        'set PHP/expose_php Off',
+        'set PHP/enable_dl Off',
+      ],
+    }) }
+    it { should contain_apache__module('php5') }
 
-  context 'on a Debian osfamily' do
-    let(:facts) { {
-      :osfamily => 'Debian',
-      :operatingsystem => 'Ubuntu',
-    } }
-    it { should include_class('php::apache::debian') }
-    it { should_not include_class('php::apache::redhat') }
+    context 'on a RedHat osfamily' do
+      let(:facts) { {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'CentOS',
+      } }
+      it { should contain_augeas('default php.ini settings')\
+        .with_incl('/etc/php.ini') }
+      it { should contain_file('/etc/httpd/mods-available/php5.load')\
+        .with_ensure('present') }
+      it { should contain_file('/etc/httpd/conf.d/php.conf')\
+        .with_ensure('absent') }
+    end
+
+    context 'on a Debian osfamily' do
+      let(:facts) { {
+        :osfamily => 'Debian',
+        :operatingsystem => 'Ubuntu',
+      } }
+      it { should contain_augeas('default php.ini settings')\
+        .with_incl('/etc/php5/apache2/php.ini') }
+      it { should contain_package('libapache2-mod-php5') }
+    end
   end
 end
