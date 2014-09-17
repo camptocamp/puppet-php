6b2aa4ef19b3302b4d82cb6df0a811071b1f01dc
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-09-17 12:21:08 +0200

    Refactor php::apache + add params for php conf
    Get rid of most class inheritance and group everything in a single class
    for much easier maintenance.

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,10 +1,58 @@
 # Install php integration with Apache
-class php::apache inherits ::php {
+class php::apache (
+  $allow_url_fopen = 'Off',
+  $expose_php = 'Off',
+  $enable_dl = 'Off',
+) inherits ::php {
 
   case $::osfamily {
-    Debian: { include ::php::apache::debian }
-    RedHat: { include ::php::apache::redhat }
+    Debian: { 
+      $php_ini = '/etc/php5/apache2/php.ini'
+    }
+    RedHat: {
+      $php_ini = '/etc/php.ini'
+
+      file { '/etc/httpd/mods-available/php5.load':
+        ensure  => present,
+        source  => 'puppet:///modules/php/httpd/php.load',
+        require => Apache_c2c::Module['php5'],
+        notify  => Service['httpd'],
+      }
+      file { '/etc/httpd/conf.d/php.conf':
+        ensure  => absent,
+        require => Apache_c2c::Module['php5'],
+        notify  => Service['httpd'],
+      }
+
+    }
     default: { notice("Unsupported osfamily ${::osfamily}")}
+  }
+
+  apache_c2c::module { 'php5':
+    ensure => present,
+    notify => Augeas['default php.ini settings'],
+  }
+
+  $_allow_url_fopen = $allow_url_fopen ? {
+    true  => 'On', false => 'Off', default => $allow_url_fopen,
+  }
+  $_expose_php = $expose_php ? {
+    true  => 'On', false => 'Off', default => $expose_php,
+  }
+  $_enable_dl = $enable_dl ? {
+    true  => 'On', false => 'Off', default => $enable_dl,
+  }
+    
+  augeas { 'default php.ini settings':
+    lens    => 'PHP.lns',
+    incl    => $php_ini,
+    changes => [
+      "set PHP/allow_url_fopen ${_allow_url_fopen}",
+      "set PHP/expose_php ${_expose_php}",
+      "set PHP/enable_dl ${_enable_dl}",
+    ],
+    require => Apache_c2c::Module['php5'],
+    notify  => Service['httpd'],
   }
 
 }
